903e286ccf6b92a02e97573ad770c25908a27fb2
Fouzia <fouziahaque.pr@gmail.com>
2016-02-19 07:29:39 -0500

    Added files via upload

---
+++ Directions.java
@@ -0,0 +1,29 @@
+package com.oniono.game;
+
+public class Directions {
+
+	static String north = "dragon";
+	static String south = "gorgon";
+	static String east = "basilisk";
+	static String west = "fairy";
+
+	public static void north() {
+
+		String dragon;
+	}
+
+	public static void south() {
+
+		String gorgon;
+	}
+
+	public static void east() {
+
+		String basilik;
+	}
+
+	public static void west() {
+
+		String fairy;
+	}
+}
